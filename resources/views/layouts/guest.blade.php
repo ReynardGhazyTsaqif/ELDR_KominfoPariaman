4206dbad78565fd36221730b763347bf4032a982
WahyuKhairi06 <WahyuKhairi06@gmail.com>
2026-07-24 22:06:57 +0700

    fix bug and title

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'ELDR Kota Pariaman') }}</title>
+        <title>{{ $title ?? 'ELDR Kota Pariaman' }}</title>
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{ asset('images/pariaman_logo.png') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'ELDR Kota Pariaman') }}</title>
+        <title>ELDR Kota Pariaman</title>
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="{{ asset('images/pariaman_logo.png') }}">
